c5c6572f5377470c1dbc6318f2838f1e078bdb1e
George Matos <georgemattos@gmail.com>
2020-04-06 17:25:37 -0300

    criacao dos componentes, ComoUsar e OndeFica dentro de Ofertas

---
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'purb-onde-fica',
+  templateUrl: './onde-fica.component.html',
+  styleUrls: ['./onde-fica.component.css']
+})
+export class OndeFicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
