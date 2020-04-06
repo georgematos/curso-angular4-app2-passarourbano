ac0c62b38855dc6c48e771a28bf48895084e00cc
George Matos <georgemattos@gmail.com>
2020-04-06 16:42:46 -0300

    criando arquivo de url da api

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -1,30 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Oferta } from '../models/oferta.model';
 import { Injectable } from '@angular/core';
+import { URL_API } from '../app.api';
 
 @Injectable()
 export class OfertasService {
-
-  private url_api = "http://localhost:3000";
 
   constructor(private http: HttpClient) {
 
   }
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get(`${this.url_api}/ofertas?destaque=true`)
+    return this.http.get(`${URL_API}/ofertas?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
 
   public getOferta(id: string): Promise<Oferta> {
-    return this.http.get(`${this.url_api}/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}/ofertas?id=${id}`)
       .toPromise()
       .then((resposta: any) => resposta.shift()); // shift remove o primeiro elemento e desloca todos os outros elementos 1 posição para esquerda
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`${this.url_api}/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta);
   }
