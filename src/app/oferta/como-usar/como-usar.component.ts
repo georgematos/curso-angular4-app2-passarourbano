a491050106e72e888d7d7c0e800a6bd555529bb9
George Matos <georgemattos@gmail.com>
2020-04-29 14:24:23 -0300

    atualizando como-uar e ond-fica no processo de mudanca de rota usando subscribe

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from 'src/app/services/ofertas.service';
 import { Oferta } from 'src/app/models/oferta.model';
 
@@ -15,11 +15,12 @@ export class ComoUsarComponent implements OnInit {
   constructor(private route: ActivatedRoute, private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
-    let id = this.route.parent.snapshot.params['id'];
-    this.ofertaService.getComoUsarOfertaPorId(id)
-      .then((resposta: string) => {
-        this.comoUsar = resposta;
-      })
+    this.route.parent.params.subscribe((params: Params) => {
+      this.ofertaService.getComoUsarOfertaPorId(params.id)
+        .then((resposta: string) => {
+          this.comoUsar = resposta;
+        })
+    })
   }
 
 }
--- src/app/oferta/onde-fica/onde-fica.component.ts
+++ src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { OfertasService } from 'src/app/services/ofertas.service';
 
 @Component({
@@ -14,11 +14,12 @@ export class OndeFicaComponent implements OnInit {
   constructor(private route: ActivatedRoute, private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
-    let id = this.route.parent.snapshot.params['id'];
-    this.ofertaService.getOndeFicaOfertaPorId(id)
-      .then((resposta) => {
-        this.ondeFica = resposta;
-      })
+    this.route.parent.params.subscribe((params: Params) => {
+      this.ofertaService.getOndeFicaOfertaPorId(params.id)
+        .then((resposta) => {
+          this.ondeFica = resposta;
+        })
+    });
   }
 
 }
