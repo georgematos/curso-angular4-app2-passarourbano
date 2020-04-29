f4ad45847fc2b789f86d78129f48da349bc7d664
George Matos <georgemattos@gmail.com>
2020-04-29 11:30:54 -0300

    pipe async (incricao em observable e promisses)

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -14,16 +14,16 @@ import { OfertasService } from '../services/ofertas.service';
 })
 export class TopoComponent implements OnInit {
 
-  public ofertasObservable: Observable<Oferta[]>;
-  public ofertas: Oferta[];
+  public ofertas: Observable<Oferta[]>;
   private subjectPesquisa: Subject<string> = new Subject<string>();
+
 
   constructor(public ofertaService: OfertasService) { }
 
   ngOnInit(): void {
-    this.ofertasObservable = this.subjectPesquisa
+    this.ofertas = this.subjectPesquisa
       .pipe(
-        debounceTime(1000), // executa a ação do switchMap após 1 segundo
+        debounceTime(600), // executa a ação do switchMap após 1 segundo
         distinctUntilChanged(),
         switchMap((termo: string) => { // será disparado sempre que o next do subjectPesquisa for chamado recebendo o argumento passado por ele
           if (termo.trim() === '') { // evita o disparo da consulta a api quando o campo estiver vazio
@@ -36,10 +36,6 @@ export class TopoComponent implements OnInit {
           return of<Oferta[]>([]);
         })
       )
-
-      this.ofertasObservable.subscribe((ofertas: Oferta[]) => {
-        this.ofertas = ofertas;
-      })
     }
 
   public pesquisa(termoDaBusca: string): void {
