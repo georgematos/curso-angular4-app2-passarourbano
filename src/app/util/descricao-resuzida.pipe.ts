d2f6885bff1276dd94cf61965ad2f954132c427d
George Matos <georgemattos@gmail.com>
2020-04-29 09:46:30 -0300

    criando um pipe customizado - parte2 - configurando parametros

--- src/app/util/descricao-resuzida.pipe.ts
+++ src/app/util/descricao-resuzida.pipe.ts
@@ -2,9 +2,9 @@ import { PipeTransform, Pipe } from '@angular/core';
 
 @Pipe({name: 'descricaoReduzida'})
 export class DescricaoReduzida implements PipeTransform {
-  transform(texto: string): string {
-    if (texto.length > 15) {
-      return texto.substr(0, 15) + '... '
+  transform(texto: string, posicaoInicial: number, posicaoFinal: number): string {
+    if (texto.length > posicaoFinal) {
+      return texto.substr(posicaoInicial, posicaoFinal) + '... '
     }
 
     return texto;
