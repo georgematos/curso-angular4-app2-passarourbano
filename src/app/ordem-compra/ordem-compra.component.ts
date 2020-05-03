7b06da6e2725146063743cfd20f0da61182d662f
George Matos <georgemattos@gmail.com>
2020-05-03 09:32:47 -0300

    two-way-binding dos dados do form

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'purb-ordem-compra',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = '';
+  public numero: string = '';
+  public complemento: string = '';
+  public formaPagamento: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  atualizarEndereco(endereco: string): void {
+    this.endereco = endereco;
+    console.log(this.endereco);
+  }
+
+  atualizarNumero(numero: string): void {
+    this.numero = numero;
+    console.log(this.numero)
+  }
+
+  atualizarComplemento(complemento: string): void {
+    this.complemento = complemento;
+    console.log(this.complemento)
+  }
+
+  atualizarFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
+    console.log(this.formaPagamento)
   }
 
 }
