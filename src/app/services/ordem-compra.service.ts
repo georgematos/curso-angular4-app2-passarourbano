4900b0680083f104670fcc61a3e52dad22423345
George Matos <georgemattos@gmail.com>
2020-05-04 10:27:44 -0300

    injetando o servico http do angular em ordem-compra.service

--- src/app/services/ordem-compra.service.ts
+++ src/app/services/ordem-compra.service.ts
@@ -1,6 +1,13 @@
 import { Pedido } from '../models/pedido.model';
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http';
 
+@Injectable()
 export class OrdemCompraService {
+
+  constructor(
+    private http: Http
+  ) { }
 
   public efetivarCompra(pedido: Pedido): void {
     console.log(pedido);
