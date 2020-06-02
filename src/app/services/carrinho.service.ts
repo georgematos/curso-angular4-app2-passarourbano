dcabe76983832fce5cbe6eb976b1fe8e7b52c4c8
George Matos <georgemattos@gmail.com>
2020-06-02 18:36:16 -0300

    criando o model ItemCarrinho e importando no CarrinhoService

--- src/app/services/carrinho.service.ts
+++ src/app/services/carrinho.service.ts
@@ -1,5 +1,7 @@
-class CarrinhoService {
+import { ItemCarrinho } from '../models/item-carrinho.model';
 
+class CarrinhoService {
+  public itens: ItemCarrinho[] = [];
 }
 
 export default CarrinhoService;
